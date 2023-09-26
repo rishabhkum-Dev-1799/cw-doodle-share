7ff6137d5583e858a05d8c5724069b75ab344ea0
rishabhkum-Dev-1799 <rishabhkum17@gmail.com>
2023-09-27 00:03:26 +0530

    "feature -Routes and Locale Creation"

---
+++ src/Routes/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Route,Routes } from 'react-router-dom'
+
+import JoinRoompage from '../pages/JoinRoompage'
+import CreateRoomPage from '../pages/CreateRoomPage'
+import Homepage from '../pages/homepage'
+import SketchRoomPage from '../pages/SketchRoomPage'
+
+const AppRoutes = () => {
+  return (
+        <Routes>
+            <Route path='/' element={<Homepage/>}/>
+            <Route path='/join-room' element={<JoinRoompage/>}/>
+            <Route path='/create-room' element={<CreateRoomPage/>}/>
+            <Route path='/:roomId' element={<SketchRoomPage/>}/>
+        </Routes>
+  )
+}
+
+export default AppRoutes;
---
+++ src/locale/en.ts
@@ -0,0 +1,20 @@
+export const ButtonLabels={
+  join_room:"Join Room",
+  create_room:"Create Room",
+  go:"Let's Go",
+  generate:"Generate",
+  copy:'Copy'
+}
+
+export const Headings={
+  join_room:"Join Room",
+  create_room:"Create Room",
+  create_room_form:"Create Room Form",
+  join_room_form:"Join Room Form"
+}
+
+export const placeholder={
+  enter_name:"Enter Your Name",
+  enter_room_code:"Enter Your Room Code",
+  generate_room_code:"Generate Room Code"
+}
